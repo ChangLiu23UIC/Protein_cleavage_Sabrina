cce15f73bc1f9d1f72bce792cfe7a2e04b378eb8
cliu239 <cliu239@uic.edu>
2024-07-19 10:07:45 -0500

    Add the scv into the project

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from utilities_cl import *
 from plot_utilities_cl import *
 import glob
+import sys
+import os
 
 def main():
+    # Path to SCV-master/python
+    scv_master_python_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'SCV-master', 'python'))
+
+    # Append the SCV-master/python path to sys.path
+    if scv_master_python_path not in sys.path:
+        sys.path.append(scv_master_python_path)
+
+    import scv_main
+
     user_input = input("Enter the list of cleavage methods separated by commas(Ex: trypsin,chymotrypsin low specificity): ")
     methods = user_input.split(',')
 
@@ -12,8 +23,6 @@ def main():
     peptide_list = peptide_list_from_protein_list(protein_sequence_list, methods)
     plot_peptide_lengths_histogram(peptide_list)
 
-    for uniprot, sequence in protein_dict.items():
-        td = two_d_image(sequence, methods)
 
     summary_statements(peptide_list, protein_sequence_list)
 
