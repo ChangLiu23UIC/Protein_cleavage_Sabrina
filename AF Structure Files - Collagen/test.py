54e6eaeb1271fbd39b2c98e691bad5c8e6a7b844
cliu239 <cliu239@uic.edu>
2024-07-26 14:16:50 -0500

    SCV with Chris' help

--- AF Structure Files - Collagen/test.py
+++ AF Structure Files - Collagen/test.py
@@ -18,7 +18,7 @@ def submit_job_with_pdb(pdb, psms, ptm_annotations, background_color, species):
 if __name__ == '__main__':
     pdb_file = "AF-A2A2Y8-F1-model_v4.pdb"  # Path to file on local machine
     psms = {'green': ['DGTEVTER', 'IVTETVTTR', 'LTSLPPK', 'GGTSNGYAK', 'TASLGGGSR', 'QSLTHGSSGYINSTGSTR', 'GHASTSSYR', 'AHSPASTLPNSPGSTFER', 'HAYEGSSSGNSSPEYPR', 'EFASSSTR', 'LQSASPSTR', 'WTELDDVK', 'SASVSPTR', 'NSSNTLPIPK', 'EMELLIMTK', 'VFTASPASIAATSFSEDTLK', 'QAAYNADSGLK'], 'blue': ['IVTASSQSVSGTYDATILDANLPSHVWSSTLPAGSSMGTYHNNMTTQSSSLLNTNAYSAGSVFGVPNNMASCSPTLHPGLSTSSSVFGMQNNLAPSLTTLSHGTTTTSTAYGVK', 'NMPQSPAAVNTGVSTSAACTTSVQSDDLLHK']}
-    ptm_annotations = {"green": [0,255,0], "blue": [0,0,255]}
+    ptm_annotations = {"green": [0,255,0], "blue": [0,0,255], "Uncovered":[255,0,0]}
     background_color = 16777215
     species = "human"
 
