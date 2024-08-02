e42509c181780cb881ed6586eada317bff22121a
cliu239 <cliu239@uic.edu>
2024-08-02 09:37:56 -0500

    SCV with Chris' help

--- utilities_cl.py
+++ utilities_cl.py
@@ -101,24 +101,6 @@ def summary_statements(peptide_list, protein_sequence_list):
 
 
 
-def read_fasta(filename):
-    """
-    A function to read the fasta file into a {Uniprot_id: Sequence} dictionary format
-    :param filename:
-    :return:
-    """
-    with open(filename, 'r') as file_read:
-        uniprot_dict = {}
-        file_content = file_read.read()
-        entries = file_content.split('>')
-        for entry in entries:
-            lines = entry.strip().split('\n')
-            header = lines[0]
-            sequence = ''.join(lines[1:])
-            uniprot_id = header.split('|')[1]
-            uniprot_dict[uniprot_id] = sequence
-        return uniprot_dict
-
 
 if __name__ == '__main__':
     file_list = glob.glob("AF Structure Files - Collagen/*.pdb")
